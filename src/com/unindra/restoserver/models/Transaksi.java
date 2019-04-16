f5f39b18679a3ed4f47636308dcba8b0ac08307a
robbyawaldi <robbyawaldi@gmail.com>
2019-04-16 17:18:26 +0700

    buat model transaksi

--- src/com/unindra/restoserver/models/Transaksi.java
+++ src/com/unindra/restoserver/models/Transaksi.java
@@ -1,6 +1,28 @@
 package com.unindra.restoserver.models;
 
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import com.unindra.restoserver.DB;
+import org.sql2o.Connection;
+
+import java.util.Date;
 
 public class Transaksi extends RecursiveTreeObject<Transaksi> {
+    private int id_transaksi;
+    private Date tanggal;
+
+    public Transaksi() {
+        tanggal = new Date();
+        try (Connection connection = DB.sql2o.open()) {
+            final String query = "INSERT INTO `transaksi` (`tanggal`) VALUES (:tanggal)";
+            id_transaksi = connection.createQuery(query).executeUpdate().getKey(Integer.class);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Transaksi{" +
+                "id_transaksi=" + id_transaksi +
+                ", tanggal=" + tanggal +
+                '}';
+    }
 }
