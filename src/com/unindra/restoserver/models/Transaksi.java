f7256b08a28c5438211faf94e4cc12f464ee7a1d
robbyawaldi <robbyawaldi@gmail.com>
2019-04-17 11:47:10 +0700

    buat method simpan di model transaksi

--- src/com/unindra/restoserver/models/Transaksi.java
+++ src/com/unindra/restoserver/models/Transaksi.java
@@ -26,11 +26,7 @@ public class Transaksi extends RecursiveTreeObject<Transaksi> {
 
     public Transaksi(String no_meja) {
         this.no_meja = no_meja;
-        tanggal = new Date();
-        try (Connection connection = DB.sql2o.open()) {
-            final String query = "INSERT INTO `transaksi` (`no_meja`,`tanggal`) VALUES (:no_meja,:tanggal)";
-            id_transaksi = connection.createQuery(query).executeUpdate().getKey(Integer.class);
-        }
+        this.tanggal = new Date();
     }
 
     private int getTotalHarga() {
@@ -39,6 +35,14 @@ public class Transaksi extends RecursiveTreeObject<Transaksi> {
                 .filter(item -> item.getNo_meja().equals(no_meja))
                 .collect(Collectors.toList());
         return items.stream().mapToInt(Item::getTotal).sum();
+    }
+
+    public boolean simpan() {
+        try (Connection connection = DB.sql2o.open()) {
+            final String query = "INSERT INTO `transaksi` (`no_meja`,`tanggal`) VALUES (:no_meja,:tanggal)";
+            id_transaksi = connection.createQuery(query).executeUpdate().getKey(Integer.class);
+            return connection.getResult() > 0;
+        }
     }
 
     public static ObservableList<Transaksi> getTransaksiList() {
