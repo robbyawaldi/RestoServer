de67443218b99c2f23cd0b7849b1b0bbc9b09fac
robbyawaldi <robbyawaldi@gmail.com>
2019-04-22 04:58:56 +0700

    buat laporan menu favorit dan kunjungan

--- src/com/unindra/restoserver/controllers/LaporanController.java
+++ src/com/unindra/restoserver/controllers/LaporanController.java
@@ -1,8 +1,13 @@
 package com.unindra.restoserver.controllers;
 
+import com.unindra.restoserver.models.Menu;
 import com.unindra.restoserver.models.Transaksi;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
 import org.joda.time.LocalDate;
 import org.joda.time.YearMonth;
@@ -11,27 +16,50 @@ import java.net.URL;
 import java.util.Date;
 import java.util.ResourceBundle;
 
+import static com.unindra.restoserver.models.Item.getItems;
+import static com.unindra.restoserver.models.Menu.getMenus;
 import static com.unindra.restoserver.models.Transaksi.getTransaksiList;
 
 public class LaporanController implements Initializable {
     public BarChart barChart;
+    public PieChart pieChart;
+    public LineChart lineChart;
 
     @SuppressWarnings("unchecked")
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        barChart.getXAxis().setLabel("Bulan");
-        barChart.getYAxis().setLabel("Pendapatan (Rp)");
         LocalDate localDate = new LocalDate(new Date());
-        XYChart.Series series = new XYChart.Series();
+
+        XYChart.Series seriesBar = new XYChart.Series();
         for (int i = 5; i > 0; i--) {
             YearMonth yearMonth = new YearMonth(localDate.minusMonths(i));
             String bulan = yearMonth.monthOfYear().getAsText() + " " + yearMonth.getYear();
-            Integer total = getTransaksiList(yearMonth.getYear(), yearMonth.getMonthOfYear())
+            Integer totalPendapatan = getTransaksiList(yearMonth.getYear(), yearMonth.getMonthOfYear())
                     .stream()
                     .mapToInt(Transaksi::getTotalHargaFromDB)
                     .sum();
-            series.getData().add(new XYChart.Data(bulan, total));
+            seriesBar.getData().add(new XYChart.Data(bulan, totalPendapatan));
         }
-        barChart.getData().addAll(series);
+        barChart.getData().addAll(seriesBar);
+        barChart.getXAxis().setLabel("Bulan");
+        barChart.getYAxis().setLabel("Pendapatan (Rp)");
+
+        ObservableList<PieChart.Data> pieObservableList = FXCollections.observableArrayList();
+        for (Menu menu : getMenus())
+            pieObservableList.add(new PieChart.Data(menu.getNama_menu(), getItems(menu).size()));
+        pieChart.setData(pieObservableList);
+        pieChart.setStartAngle(90);
+
+
+        XYChart.Series seriesLine = new XYChart.Series();
+        for (int i = 5; i > 0; i--) {
+            YearMonth yearMonth = new YearMonth(localDate.minusMonths(i));
+            String bulan = yearMonth.monthOfYear().getAsText() + " " + yearMonth.getYear();
+            Integer totalKunjungan = getTransaksiList(yearMonth.getYear(), yearMonth.getMonthOfYear()).size();
+            seriesLine.getData().add(new XYChart.Data<>(bulan, totalKunjungan));
+        }
+        lineChart.getData().addAll(seriesLine);
+        lineChart.getXAxis().setLabel("Bulan");
+        lineChart.getYAxis().setLabel("Kunjungan");
     }
 }
