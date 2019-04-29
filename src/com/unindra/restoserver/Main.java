90ea455a402f465d632c4fd03f925957f37f5cb5
robbyawaldi <robbyawaldi@gmail.com>
2019-04-29 14:23:23 +0700

    merapihkan class Main

--- src/com/unindra/restoserver/Main.java
+++ src/com/unindra/restoserver/Main.java
@@ -8,7 +8,6 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception {
         new Server();
