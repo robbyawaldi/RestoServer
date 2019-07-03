2c59a385024fb8f9af40b37d761e0cdebef6be76
robbyawaldi <robbyawaldi@gmail.com>
2019-07-03 16:39:01 +0700

    ubah db class

--- src/com/unindra/restoserver/DB.java
+++ src/com/unindra/restoserver/DB.java
@@ -1,16 +1,46 @@
 package com.unindra.restoserver;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 import org.sql2o.Sql2o;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class DB {
     public static Sql2o sql2o;
 
     static {
-        sql2o = new Sql2o("jdbc:mysql://localhost/osaka", "root", "zxcasdqwe123");
         try {
-            sql2o.open();
-        } catch (Exception e) {
-            sql2o = new Sql2o("jdbc:mysql://localhost/osaka", "root", "");
+            DBConfig config =  new Gson().fromJson(new JsonReader(new FileReader(new File("db.json"))), DBConfig.class);
+            sql2o = new Sql2o(config.getHost(), config.getUser(), config.getPass());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class DBConfig {
+        private String host;
+        private String user;
+        private String pass;
+
+        public DBConfig(String host, String user, String pass) {
+            this.host = host;
+            this.user = user;
+            this.pass = pass;
+        }
+
+        String getHost() {
+            return host;
+        }
+
+        String getUser() {
+            return user;
+        }
+
+        String getPass() {
+            return pass;
         }
     }
 }
